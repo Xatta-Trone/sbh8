c701753fac1ed3b20828cfa1d0cc343aa3a0ee1d
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-10-21 01:07:42 +0900

    user nave updated

--- resources/views/user/shared/nav.blade.php
+++ resources/views/user/shared/nav.blade.php
@@ -1,16 +1,19 @@
 <nav class="nav-section">
         <div class="container">
-            <div class="nav-right">
+            {{-- <div class="nav-right">
                 <a href=""><i class="fa fa-search"></i></a>
                 <a href=""><i class="fa fa-shopping-cart"></i></a>
-            </div>
+            </div> --}}
             <ul class="main-menu">
                 <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="#">Event</a></li>
-                <li><a href="course.html">COURSES</a></li>
-                <li><a href="blog.html">blog</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Administration</a></li>
+                <li><a href="#">Residents</a></li>
+                <li><a href="#">Activities</a></li>
+                <li><a href="#">Alumni</a></li>
+                <li><a href="#">Notice Board</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
         </div>
     </nav>
