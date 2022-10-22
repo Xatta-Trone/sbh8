ed0437839a472c03dc962882272f7569241d2380
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-10-23 03:33:24 +0900

    footer done

--- resources/views/user/shared/footer.blade.php
+++ resources/views/user/shared/footer.blade.php
@@ -1,86 +1,66 @@
 <footer class="footer-section mt-5">
-        <div class="container footer-top">
-            <div class="row">
-                <!-- widget -->
-                <div class="col-sm-6 col-lg-3 footer-widget">
-                    <div class="about-widget">
-                        <img src="{{ asset('user_assets/img/logo-light.png') }}" alt="">
-                        <p>orem ipsum dolor sit amet, consecter adipiscing elite. Donec minos varius, viverra justo ut,
-                            aliquet nisl.</p>
-                        <div class="social pt-1">
-                            <a href=""><i class="fa fa-twitter-square"></i></a>
-                            <a href=""><i class="fa fa-facebook-square"></i></a>
-                            <a href=""><i class="fa fa-google-plus-square"></i></a>
-                            <a href=""><i class="fa fa-linkedin-square"></i></a>
-                            <a href=""><i class="fa fa-rss-square"></i></a>
-                        </div>
-                    </div>
+    <div class="container footer-top">
+        <div class="row">
+            <!-- widget -->
+            <div class="col-xs-12 col-sm-12 col-lg-4 footer-widget">
+                <div class="about-widget">
+                    <img src="{{ $logo }}" alt="{{ $name_short }}">
+                    <p>{{ $name_short }} <br> {{ $description_short }}</p>
+
                 </div>
-                <!-- widget -->
-                <div class="col-sm-6 col-lg-3 footer-widget">
-                    <h6 class="fw-title">USEFUL LINK</h6>
-                    <div class="dobule-link">
-                        <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">About us</a></li>
-                            <li><a href="">Services</a></li>
-                            <li><a href="">Events</a></li>
-                            <li><a href="">Features</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="">Policy</a></li>
-                            <li><a href="">Term</a></li>
-                            <li><a href="">Help</a></li>
-                            <li><a href="">FAQs</a></li>
-                            <li><a href="">Site map</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- widget -->
-                <div class="col-sm-6 col-lg-3 footer-widget">
-                    <h6 class="fw-title">RECENT POST</h6>
-                    <ul class="recent-post">
-                        <li>
-                            <p>Snackable study:How to break <br> up your master's degree</p>
-                            <span><i class="fa fa-clock-o"></i>24 Mar 2018</span>
-                        </li>
-                        <li>
-                            <p>Open University plans major <br> cuts to number of staff</p>
-                            <span><i class="fa fa-clock-o"></i>24 Mar 2018</span>
-                        </li>
+            </div>
+            <!-- widget -->
+            <div class="col-xs-12 col-sm-12 col-lg-4 footer-widget">
+                <h6 class="fw-title">USEFUL LINK</h6>
+                <div class="dobule-link">
+                    <ul>
+                        <li><a href="{{ route('home') }}">Home</a></li>
+                        <li><a href="/about">About us</a></li>
+                        <li><a href="/history">History</a></li>
+                        <li><a href="{{ route('administration') }}">Administration</a></li>
+                        <li><a href="/residents">Residents</a></li>
                     </ul>
-                </div>
-                <!-- widget -->
-                <div class="col-sm-6 col-lg-3 footer-widget">
-                    <h6 class="fw-title">CONTACT</h6>
-                    <ul class="contact">
-                        <li>
-                            <p><i class="fa fa-map-marker"></i> 40 Baria Street 133/2, NewYork City,US</p>
-                        </li>
-                        <li>
-                            <p><i class="fa fa-phone"></i> (+88) 111 555 666</p>
-                        </li>
-                        <li>
-                            <p><i class="fa fa-envelope"></i> infodeercreative@gmail.com</p>
-                        </li>
-                        <li>
-                            <p><i class="fa fa-clock-o"></i> Monday - Friday, 08:00AM - 06:00 PM</p>
-                        </li>
+                    <ul>
+                        <li><a href="/activities">Activities</a></li>
+                        <li><a href="{{ route('alumni') }}">Alumins</a></li>
+                        <li><a href="{{ route('notice') }}">Notice Board</a></li>
+                        <li><a href="{{ route('contact') }}">Contact</a></li>
+                        <li><a href="/sitemap.xml">Site map</a></li>
                     </ul>
                 </div>
             </div>
-        </div>
-        <!-- copyright -->
-        <div class="copyright">
-            <div class="container">
-                <p>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script> All rights reserved Sher-E-Banlga Hall, BUET | This website is developed by <a href="https://www.facebook.com/monzurul.islam1112/" target="_blank">Monzurul ISLAM</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
+
+            <!-- widget -->
+            <div class="col-xs-12 col-sm-12 col-lg-4 footer-widget">
+                <h6 class="fw-title">CONTACT</h6>
+                <ul class="contact">
+                    <li>
+                        <p><i class="fa fa-map-marker"></i> {{ $address }}</p>
+                    </li>
+                    <li>
+                        <p><i class="fa fa-phone"></i> {{ $phone }}</p>
+                    </li>
+                    <li>
+                        <p><i class="fa fa-envelope"></i> {{ $email }}</p>
+                    </li>
+
+                </ul>
             </div>
         </div>
-    </footer>
+    </div>
+    <!-- copyright -->
+    <div class="copyright">
+        <div class="container">
+            <p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script> All rights reserved Sher-E-Banlga Hall, BUET | This website is developed by <a
+                    href="https://www.facebook.com/monzurul.islam1112/" target="_blank">Monzurul ISLAM</a> | Template by <a href="https://colorlib.com"
+                    target="_blank">Colorlib</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+        </div>
+    </div>
+</footer>
